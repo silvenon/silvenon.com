3301cbbbcd17a50f35e5a66107a484a898d8dc7c
Matija Marohnić <matija.marohnic@gmail.com>
2017-12-03 17:34:35 +0100

    Update biography

--- constants.js
+++ constants.js
@@ -15,7 +15,7 @@ export const BREAKPOINT = '40rem'
 export const AVATAR_SIZE = 10
 export const BIO = "I'm a design-savvy frontend developer from Croatia."
 export const BIOGRAPHY = `
-I'm a design-savvy frontend developer from Croatia. I love React.js and enjoy creating delightful, maintainable UIs.
+I'm a design-savvy frontend developer from Croatia. I love React and enjoy creating delightful, maintainable UIs.
 `.trim()
 export const COLOR = {
   BLUE_LIGHTER: '#007FAA',
