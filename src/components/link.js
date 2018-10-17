e826ac6e654c787e4a61bf1895c0769bdc022f16
Matija Marohnić <matija.marohnic@gmail.com>
2018-10-17 23:19:17 +0200

    Avoid Gatsby's Link component only in production

--- src/components/link.js
+++ src/components/link.js
@@ -1,6 +1,6 @@
 // @flow
 import * as React from 'react'
-// import { Link } from 'gatsby'
+import { Link as GatsbyLink } from 'gatsby'
 import withClassNames from './with-class-names'
 import styles from './link.module.css'
 
@@ -9,13 +9,18 @@ type Props = {
   children: React.Node,
 }
 
-// avoiding Gatsby's <Link> until style duplication gets resolved
+// avoiding Gatsby's <Link> in production until style duplication gets resolved
 // https://github.com/gatsbyjs/gatsby/issues/7517
 
-const Link = ({ to, children, ...props }: Props) => (
-  <a href={to} {...props}>
-    {children}
-  </a>
-)
+const Link = ({ to, children, ...props }: Props) =>
+  process.env.NODE_ENV === 'production' ? (
+    <a href={to} {...props}>
+      {children}
+    </a>
+  ) : (
+    <GatsbyLink to={to} {...props}>
+      {children}
+    </GatsbyLink>
+  )
 
 export default withClassNames(styles.link)(Link)
