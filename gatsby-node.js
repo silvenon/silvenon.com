97991bd6d56710094f018abcb56a7223059da0b4
Matija Marohnić <matija.marohnic@gmail.com>
2018-11-11 22:26:03 +0100

    Extract paginate function into a separate file

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,7 +5,7 @@
  */
 
 const path = require('path')
-const { range } = require('lodash')
+const paginate = require('./utils/paginate')
 
 exports.onCreateBabelConfig = ({ actions }) => {
   actions.setBabelPlugin(
@@ -74,38 +74,6 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: isDraft,
     })
   }
-}
-
-const paginate = ({
-  createPage,
-  basePath,
-  component,
-  items,
-  perPage,
-  context,
-}) => {
-  const pages = range(Math.ceil(items.length / perPage))
-  const getPagePath = page =>
-    page === 0 ? basePath : `${basePath}/page/${page + 1}`
-  pages.forEach(page => {
-    const currentPagePath = getPagePath(page)
-    const previousPagePath = page > 0 ? getPagePath(page - 1) : null
-    const nextPagePath = page < pages.length - 1 ? getPagePath(page + 1) : null
-
-    createPage({
-      path: currentPagePath,
-      component,
-      context: {
-        ...context,
-        limit: perPage,
-        skip: perPage * page,
-        pageNumber: page,
-        numberOfPages: pages.length,
-        previousPagePath,
-        nextPagePath,
-      },
-    })
-  })
 }
 
 exports.createPages = async ({ graphql, actions }) => {
---
+++ utils/paginate.js
@@ -0,0 +1,35 @@
+const { range } = require('lodash')
+
+const paginate = ({
+  createPage,
+  basePath,
+  component,
+  items,
+  perPage,
+  context,
+}) => {
+  const pages = range(Math.ceil(items.length / perPage))
+  const getPagePath = page =>
+    page === 0 ? basePath : `${basePath}/page/${page + 1}`
+  pages.forEach(page => {
+    const currentPagePath = getPagePath(page)
+    const previousPagePath = page > 0 ? getPagePath(page - 1) : null
+    const nextPagePath = page < pages.length - 1 ? getPagePath(page + 1) : null
+
+    createPage({
+      path: currentPagePath,
+      component,
+      context: {
+        ...context,
+        limit: perPage,
+        skip: perPage * page,
+        pageNumber: page,
+        numberOfPages: pages.length,
+        previousPagePath,
+        nextPagePath,
+      },
+    })
+  })
+}
+
+module.exports = paginate
