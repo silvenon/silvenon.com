e47239a7c75bbb0ab31acf93a02f00572b04b13b
Matija Marohnić <matija.marohnic@gmail.com>
2019-01-06 12:19:22 +0100

    Use Gatsby Babel preset

--- babel.config.js
+++ babel.config.js
@@ -1,40 +1,5 @@
 // https://www.gatsbyjs.org/docs/babel/#how-to-use-a-custom-babelrc-file
 
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        loose: true,
-        modules: process.env.NODE_ENV === 'test' ? 'commonjs' : false,
-        useBuiltIns: 'usage',
-        shippedProposals: true,
-      },
-    ],
-    [
-      '@babel/preset-react',
-      {
-        useBuiltIns: true,
-        pragma: 'React.createElement',
-      },
-    ],
-    '@babel/preset-flow',
-  ],
-  plugins: [
-    [
-      '@babel/plugin-proposal-class-properties',
-      {
-        loose: true,
-      },
-    ],
-    '@babel/plugin-syntax-dynamic-import',
-    'babel-plugin-macros',
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        helpers: true,
-        regenerator: true,
-      },
-    ],
-  ],
+  presets: ['babel-preset-gatsby'],
 }
