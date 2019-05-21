e03f61c9827a002f85ab73ee5128b66275d3f867
Matija Marohnić <matija.marohnic@gmail.com>
2019-05-21 10:27:56 +0200

    Use Gatsby Link component in production too
    Now Gatsby no longer has issues with duplicating styles when preloading.
    
    https://github.com/gatsbyjs/gatsby/issues/7517

--- src/components/link.js
+++ src/components/link.js
@@ -1,26 +1,8 @@
 // @flow
-import React, { type Node } from 'react'
 import { Link as GatsbyLink } from 'gatsby'
 import withClassNames from './with-class-names'
 import styles from './link.module.css'
 
-type Props = {
-  to: string,
-  children: Node,
-}
-
-// avoiding Gatsby's <Link> in production until style duplication gets resolved
-// https://github.com/gatsbyjs/gatsby/issues/7517
-
-const Link = ({ to, children, ...props }: Props) =>
-  process.env.NODE_ENV === 'production' ? (
-    <a href={to} {...props}>
-      {children}
-    </a>
-  ) : (
-    <GatsbyLink to={to} {...props}>
-      {children}
-    </GatsbyLink>
-  )
+const Link = GatsbyLink
 
 export default withClassNames(styles.link)(Link)
