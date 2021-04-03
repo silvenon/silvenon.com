d1809166f6f414b4f364f7bbfdec432627d224b5
Matija Marohnić <matija.marohnic@gmail.com>
2021-04-03 17:35:51 +0200

    Fix lint script

--- .eslintrc.js
+++ .eslintrc.js
@@ -70,6 +70,7 @@ module.exports = {
         '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-extra-semi': 'off',
         '@typescript-eslint/no-var-requires': 'off',
         '@typescript-eslint/no-empty-interface': [
           'error',
