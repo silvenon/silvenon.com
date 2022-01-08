a2ac65f45ea6fee647dc9afb24cb9467849879f2
Matija Marohnić <matija.marohnic@gmail.com>
2022-01-08 03:46:34 +0100

    Set up deployment to Fly.io

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -14,6 +14,17 @@ export default function handleRequest(
 
   responseHeaders.set('Content-Type', 'text/html')
 
+  const requestURL = new URL(request.url)
+  const proto = request.headers.get('X-Forwarded-Proto')
+  const host =
+    request.headers.get('X-Forwarded-Host') ?? request.headers.get('host')
+
+  if (proto === 'http') {
+    responseStatusCode = 301
+    responseHeaders.set('X-Forwarded-Proto', 'https')
+    responseHeaders.set('location', 'https://' + host + requestURL.pathname)
+  }
+
   return new Response('<!DOCTYPE html>' + markup, {
     status: responseStatusCode,
     headers: responseHeaders,
