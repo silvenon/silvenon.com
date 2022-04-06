e0cc782c732b81d1ef6519ee9c28ff045483049e
Matija Marohnić <matija.marohnic@gmail.com>
2022-04-06 02:18:44 +0200

    Revert "Import ESM-only packages normally"
    This only works in development, unfortunately, and
    `serverDependenciesToBundle` causes these plugins to not have effect in
    development, so dynamic import it is.
    
    This reverts commit e42d08eb76ba5d1f6733f078b89f5bb805763cdd.

--- app/utils/mdx.server.ts
+++ app/utils/mdx.server.ts
@@ -3,10 +3,14 @@ import { configureRehypePrettyCode } from './rehype-pretty-code'
 import { esbuildPluginBrowserslist } from 'esbuild-plugin-browserslist'
 import browserslist from 'browserslist'
 import esbuildPluginCloudinary from './esbuild-plugin-cloudinary.server'
-import remarkSmartypants from 'remark-smartypants'
-import remarkUnwrapImages from 'remark-unwrap-images'
 
 export async function bundleMDXPost(content: string) {
+  // the official recommendation for importing ESM-only packages is to use serverDependenciesToBundle
+  // howeever, that causes these plugins not tu have effect in development for some reason
+  // https://remix.run/docs/en/v1/pages/gotchas#importing-esm-packages
+  const { default: remarkSmartypants } = await import('remark-smartypants')
+  const { default: remarkUnwrapImages } = await import('remark-unwrap-images')
+
   const configuredRehypePrettyCode = configureRehypePrettyCode()
 
   const { code, errors } = await bundleMDX({
