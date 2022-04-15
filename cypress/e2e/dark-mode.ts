8051cb51378c5ded00396ac4829e4f177923cd6d
Matija Marohnić <matija.marohnic@gmail.com>
2022-04-16 00:00:15 +0200

    Make dark mode test stable
    Locally the test was passing just because I happened to be testing at
    night, when my OS had dark mode turned on anyway, but CI was failing
    because it didn't. The problem was that after `cy.reload()`
    window.matchMedia was no longer stubbed, so stubbing it on
    `window:before:load` fixes the issue.

--- cypress/e2e/dark-mode.ts
+++ cypress/e2e/dark-mode.ts
@@ -1,38 +1,39 @@
 describe('dark mode', () => {
   it('toggle', () => {
-    cy.visit('/', {
-      onBeforeLoad(win) {
-        cy.stub(win, 'matchMedia')
-          .withArgs('(prefers-color-scheme: dark)')
-          .returns({
-            matches: true,
-            addEventListener() {},
-          })
-      },
+    // works both on visit() and reload()
+    cy.on('window:before:load', (win) => {
+      cy.stub(win, 'matchMedia')
+        .withArgs('(prefers-color-scheme: dark)')
+        .returns({
+          matches: false,
+          addEventListener() {},
+        })
     })
 
-    cy.get('html').should('have.class', 'dark')
+    cy.visit('/')
+
+    cy.get('html').should('not.have.class', 'dark')
     cy.findByRole('button', { name: 'Reset to OS' }).should('not.exist')
 
-    cy.findByRole('switch', { name: 'Disable dark mode' }).click()
-    cy.get('html').should('not.have.class', 'dark')
-    cy.findByRole('button', { name: 'Reset to OS' }).should('exist')
     cy.findByRole('switch', { name: 'Enable dark mode' }).click()
     cy.get('html').should('have.class', 'dark')
     cy.findByRole('button', { name: 'Reset to OS' }).should('exist')
     cy.findByRole('switch', { name: 'Disable dark mode' }).click()
     cy.get('html').should('not.have.class', 'dark')
+    cy.findByRole('button', { name: 'Reset to OS' }).should('exist')
 
+    cy.findByRole('switch', { name: 'Enable dark mode' }).click()
+    cy.get('html').should('have.class', 'dark')
     cy.reload()
-    cy.get('html').should('not.have.class', 'dark')
+    cy.get('html').should('have.class', 'dark')
     cy.findByRole('button', { name: 'Reset to OS' }).should('exist')
 
     cy.findByRole('button', { name: 'Reset to OS' }).click()
-    cy.get('html').should('have.class', 'dark')
+    cy.get('html').should('not.have.class', 'dark')
     cy.findByRole('button', { name: 'Reset to OS' }).should('not.exist')
 
     cy.reload()
-    cy.get('html').should('have.class', 'dark')
+    cy.get('html').should('not.have.class', 'dark')
     cy.findByRole('button', { name: 'Reset to OS' }).should('not.exist')
   })
 })
