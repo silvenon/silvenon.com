c36331cbf0473235f998d8f4cc5b1c8a929276db
Matija Marohnić <matija.marohnic@gmail.com>
2022-04-27 12:29:53 +0200

    Fix recompiling posts on file change

--- scripts/compile-posts.ts
+++ scripts/compile-posts.ts
@@ -5,103 +5,98 @@ import browserslist from 'browserslist'
 import esbuildPluginCloudinary from './utils/esbuild-plugin-cloudinary'
 import fs from 'fs/promises'
 import path from 'path'
+import chokidar from 'chokidar'
 
 const SOURCE_DIR = `${__dirname}/../posts`
 const OUTPUT_DIR = `${__dirname}/../app/posts`
 
-async function buildPosts() {
+async function compilePosts() {
+  const dirents = await fs.readdir(`${__dirname}/../posts`, {
+    withFileTypes: true,
+  })
+
+  const postFiles: string[] = []
+
+  for (const dirent of dirents) {
+    if (dirent.name === '__tests__') continue
+    if (dirent.isFile()) {
+      if (!dirent.name.endsWith('.mdx')) continue
+      postFiles.push(dirent.name)
+    } else {
+      const seriesSlug = dirent.name
+      const partDirents = await fs.readdir(path.join(SOURCE_DIR, seriesSlug), {
+        withFileTypes: true,
+      })
+      for (const partDirent of partDirents) {
+        if (
+          partDirent.name === 'series.json' ||
+          partDirent.name.endsWith('.mdx')
+        ) {
+          postFiles.push(`${seriesSlug}/${partDirent.name}`)
+        }
+      }
+    }
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    const watcher = chokidar.watch(['**/*.mdx', '**/series.json'], {
+      cwd: SOURCE_DIR,
+    })
+    watcher.on('change', (path) => compile(path))
+    console.log('Watching posts for changes...')
+  } else {
+    await fs.rm(OUTPUT_DIR, { recursive: true, force: true })
+    console.log('Compiling posts...')
+    console.time('Compiled posts')
+    await Promise.all(postFiles.map((file) => compile(file)))
+    console.timeEnd('Compiled posts')
+  }
+}
+
+async function compile(file: string) {
+  if (path.basename(file) === 'series.json') {
+    const outputPath = path.join(OUTPUT_DIR, file)
+    await fs.mkdir(path.dirname(outputPath), { recursive: true })
+    await fs.copyFile(path.join(SOURCE_DIR, file), outputPath)
+    return
+  }
+
   const [{ default: remarkSmartypants }, { default: remarkUnwrapImages }] =
     await Promise.all([
       import('remark-smartypants'),
       import('remark-unwrap-images'),
     ])
 
-  const dirents = await fs.readdir(`${__dirname}/../posts`, {
-    withFileTypes: true,
+  const { code } = await bundleMDX({
+    file: path.join(SOURCE_DIR, file),
+    cwd: path.dirname(path.join(SOURCE_DIR, file)),
+    mdxOptions: (options) => {
+      options.remarkPlugins = [
+        ...(options.remarkPlugins || []),
+        remarkSmartypants,
+        remarkUnwrapImages,
+      ]
+      options.rehypePlugins = [
+        ...(options.rehypePlugins || []),
+        ...rehypePrettyCodePlugins,
+      ]
+      return options
+    },
+    esbuildOptions: (options) => {
+      options.plugins = [
+        esbuildPluginBrowserslist(browserslist(), {
+          printUnknownTargets: false,
+        }),
+        esbuildPluginCloudinary,
+        ...(options.plugins ?? []),
+      ]
+      return options
+    },
   })
 
-  const posts = []
-
-  for (const dirent of dirents) {
-    if (dirent.name === '__tests__') continue
-    if (dirent.isFile()) {
-      if (!dirent.name.endsWith('.mdx')) continue
-      posts.push(dirent.name)
-    } else {
-      const seriesSlug = dirent.name
-      const partDirents = await fs.readdir(`${SOURCE_DIR}/${seriesSlug}`, {
-        withFileTypes: true,
-      })
-      for (const partDirent of partDirents) {
-        if (!partDirent.name.endsWith('.mdx')) continue
-        posts.push(`${seriesSlug}/${partDirent.name}`)
-      }
-    }
-  }
-
-  await fs.rm(OUTPUT_DIR, { recursive: true, force: true })
-
-  await Promise.all(
-    posts.map(async (post) => {
-      const { code } = await bundleMDX({
-        file: path.join(SOURCE_DIR, post),
-        cwd: path.dirname(path.join(SOURCE_DIR, post)),
-        mdxOptions: (options) => {
-          options.remarkPlugins = [
-            ...(options.remarkPlugins || []),
-            remarkSmartypants,
-            remarkUnwrapImages,
-          ]
-          options.rehypePlugins = [
-            ...(options.rehypePlugins || []),
-            ...rehypePrettyCodePlugins,
-          ]
-          return options
-        },
-        esbuildOptions: (options) => {
-          options.plugins = [
-            esbuildPluginBrowserslist(browserslist(), {
-              printUnknownTargets: false,
-            }),
-            esbuildPluginCloudinary,
-            ...(options.plugins ?? []),
-          ]
-          options.watch =
-            process.env.NODE_ENV === 'development'
-              ? {
-                  async onRebuild(error, result) {
-                    if (error) {
-                      console.error(`Recompiling posts/${post} failed:`, error)
-                      return
-                    }
-                    if (!result?.outputFiles) {
-                      console.error(`Missing outputFiles for posts/${post}`)
-                      return
-                    }
-                    await writeResult(result.outputFiles[0].text)
-                    console.log(`Recompiled posts/${post}`)
-                  },
-                }
-              : false
-          return options
-        },
-      })
-
-      await writeResult(code)
-
-      async function writeResult(content: string) {
-        const outputPath = path.join(OUTPUT_DIR, post).replace(/\.mdx$/, '.js')
-        await fs.mkdir(path.dirname(outputPath), { recursive: true })
-        await fs.writeFile(outputPath, content)
-      }
-    }),
-  )
-
-  console.log(
-    process.env.NODE_ENV === 'development'
-      ? `Compiled posts, watching for changes...`
-      : `Compiled posts`,
-  )
+  const outputPath = path.join(OUTPUT_DIR, file).replace(/\.mdx$/, '.js')
+  await fs.mkdir(path.dirname(outputPath), { recursive: true })
+  await fs.writeFile(outputPath, code)
 }
 
-buildPosts()
+compilePosts()
