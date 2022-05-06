4f139f398f2d319d2858272da18629f5d7ec83f4
Matija Marohnić <matija.marohnic@gmail.com>
2022-05-06 19:27:34 +0200

    Remove necessity for explicit Vite dependency

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,6 @@
 /// <reference types="vitest" />
-/// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
