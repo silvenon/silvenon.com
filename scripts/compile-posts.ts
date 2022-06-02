7408e9d68a9c0606389ef712d4cdd7941f81685f
Matija Marohnić <matija.marohnic@gmail.com>
2022-06-02 02:28:40 +0200

    Optimize compiling posts

--- scripts/compile-posts.ts
+++ scripts/compile-posts.ts
@@ -4,35 +4,65 @@ import { esbuildPluginBrowserslist } from 'esbuild-plugin-browserslist'
 import browserslist from 'browserslist'
 import esbuildPluginCloudinary from './utils/esbuild-plugin-cloudinary'
 import fs from 'fs/promises'
+import { createWriteStream } from 'fs'
 import path from 'path'
 import chokidar from 'chokidar'
+import cacache from 'cacache'
 
-const SOURCE_DIR = `${__dirname}/../posts`
-const OUTPUT_DIR = `${__dirname}/../app/posts`
+const SOURCE_DIR = path.join(process.cwd(), 'posts')
+const OUTPUT_DIR = path.join(process.cwd(), 'app/posts')
+const CACHE_DIR = path.join(process.cwd(), 'node_modules/.cache/posts')
 
 async function compilePosts() {
-  const dirents = await fs.readdir(`${__dirname}/../posts`, {
-    withFileTypes: true,
-  })
+  // if any of these files change we need to clear the cache
+  const scripts = [
+    'compile-posts.ts',
+    'utils/rehype-pretty-code.ts',
+    'utils/esbuild-plugin-cloudinary.ts',
+    'utils/cloudinary.ts',
+    'utils/code-theme.js',
+  ]
 
+  if (
+    (
+      await Promise.all(
+        scripts.map(async (file) =>
+          cacache.get.info(
+            path.join(CACHE_DIR, 'scripts', file),
+            path.join(
+              'scripts',
+              `${file}-${await fs
+                .stat(path.join(__dirname, file))
+                .then((stat) => stat.mtimeMs)}`,
+            ),
+          ),
+        ),
+      )
+    )
+      // this is a mistake in the type definition, CacheObject can be null
+      // @ts-expect-error
+      .includes(null)
+  ) {
+    await cacache.rm.all(CACHE_DIR)
+  }
+
+  const dirents = await cachedReaddir(SOURCE_DIR)
   const postFiles: string[] = []
 
   for (const dirent of dirents) {
     if (dirent.name === '__tests__') continue
-    if (dirent.isFile()) {
+    if (dirent.isFile) {
       if (!dirent.name.endsWith('.mdx')) continue
       postFiles.push(dirent.name)
     } else {
       const seriesSlug = dirent.name
-      const partDirents = await fs.readdir(path.join(SOURCE_DIR, seriesSlug), {
-        withFileTypes: true,
-      })
+      const partDirents = await cachedReaddir(path.join(SOURCE_DIR, seriesSlug))
       for (const partDirent of partDirents) {
         if (
           partDirent.name === 'series.json' ||
           partDirent.name.endsWith('.mdx')
         ) {
-          postFiles.push(`${seriesSlug}/${partDirent.name}`)
+          postFiles.push(path.join(seriesSlug, partDirent.name))
         }
       }
     }
@@ -54,49 +84,93 @@ async function compilePosts() {
 }
 
 async function compile(file: string) {
+  await fs.mkdir(path.dirname(path.join(OUTPUT_DIR, file)), { recursive: true })
+
   if (path.basename(file) === 'series.json') {
     const outputPath = path.join(OUTPUT_DIR, file)
-    await fs.mkdir(path.dirname(outputPath), { recursive: true })
     await fs.copyFile(path.join(SOURCE_DIR, file), outputPath)
     return
   }
 
-  const [{ default: remarkSmartypants }, { default: remarkUnwrapImages }] =
-    await Promise.all([
-      import('remark-smartypants'),
-      import('remark-unwrap-images'),
-    ])
-
-  const { code } = await bundleMDX({
-    file: path.join(SOURCE_DIR, file),
-    cwd: path.dirname(path.join(SOURCE_DIR, file)),
-    mdxOptions: (options) => {
-      options.remarkPlugins = [
-        ...(options.remarkPlugins || []),
-        remarkSmartypants,
-        remarkUnwrapImages,
-      ]
-      options.rehypePlugins = [
-        ...(options.rehypePlugins || []),
-        ...rehypePrettyCodePlugins,
-      ]
-      return options
-    },
-    esbuildOptions: (options) => {
-      options.plugins = [
-        esbuildPluginBrowserslist(browserslist(), {
-          printUnknownTargets: false,
-        }),
-        esbuildPluginCloudinary,
-        ...(options.plugins ?? []),
-      ]
-      return options
-    },
-  })
-
+  const cachePath = path.join(CACHE_DIR, file)
+  const lastModified = await fs
+    .stat(path.join(SOURCE_DIR, file))
+    .then((stat) => stat.mtimeMs)
+  const cacheKey = `${path.join('posts', file)}-${lastModified}`
   const outputPath = path.join(OUTPUT_DIR, file).replace(/\.mdx$/, '.js')
-  await fs.mkdir(path.dirname(outputPath), { recursive: true })
-  await fs.writeFile(outputPath, code)
+
+  try {
+    await new Promise<void>((resolve, reject) => {
+      try {
+        cacache.get
+          .stream(cachePath, cacheKey)
+          .on('end', resolve)
+          .on('error', reject)
+          .pipe(createWriteStream(outputPath))
+      } catch (err) {
+        reject(err)
+      }
+    })
+  } catch (err) {
+    const [{ default: remarkSmartypants }, { default: remarkUnwrapImages }] =
+      await Promise.all([
+        import('remark-smartypants'),
+        import('remark-unwrap-images'),
+      ])
+
+    const { code } = await bundleMDX({
+      file: path.join(SOURCE_DIR, file),
+      cwd: path.dirname(path.join(SOURCE_DIR, file)),
+      mdxOptions: (options) => {
+        options.remarkPlugins = [
+          ...(options.remarkPlugins || []),
+          remarkSmartypants,
+          remarkUnwrapImages,
+        ]
+        options.rehypePlugins = [
+          ...(options.rehypePlugins || []),
+          ...rehypePrettyCodePlugins,
+        ]
+        return options
+      },
+      esbuildOptions: (options) => {
+        options.plugins = [
+          esbuildPluginBrowserslist(browserslist(), {
+            printUnknownTargets: false,
+          }),
+          esbuildPluginCloudinary,
+          ...(options.plugins ?? []),
+        ]
+        return options
+      },
+    })
+
+    await fs.writeFile(outputPath, code)
+    await cacache.put(cachePath, cacheKey, code)
+  }
+}
+
+async function cachedReaddir(
+  dir: string,
+): Promise<Array<{ name: string; isFile: boolean }>> {
+  const cachePath = path.join(CACHE_DIR, path.relative(SOURCE_DIR, dir))
+  const lastModified = await fs.stat(dir).then((stat) => stat.mtimeMs)
+  const cacheKey = `${path.relative(process.cwd(), dir)}-${lastModified}`
+  return cacache
+    .get(cachePath, cacheKey)
+    .then((obj) => JSON.parse(obj.data.toString('utf-8')))
+    .catch(async () => {
+      const dirents = (
+        await fs.readdir(dir, {
+          withFileTypes: true,
+        })
+      ).map((dirent) => ({
+        name: dirent.name,
+        isFile: dirent.isFile(),
+      }))
+      await cacache.put(cachePath, cacheKey, JSON.stringify(dirents))
+      return dirents
+    })
 }
 
 compilePosts()
