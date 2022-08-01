222fa87e6e37c46c07b6803f017325c6049f60c4
Matija Marohnić <matija.marohnic@gmail.com>
2022-08-01 19:16:29 +0200

    Run Prettier separately from ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     '@remix-run/eslint-config/node',
     '@remix-run/eslint-config/jest',
     '@remix-run/eslint-config/jest-testing-library',
-    'plugin:prettier/recommended',
+    'prettier',
   ],
   // we're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but we have to explicitly
