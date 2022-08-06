ad642746cbf903bce4be34345234e07bcad5ac46
Matija Marohnić <matija.marohnic@gmail.com>
2022-08-06 17:48:40 +0200

    Track page view when location changes

--- app/components/Analytics.tsx
+++ app/components/Analytics.tsx
@@ -1,15 +1,26 @@
 import { useEffect } from 'react'
-import { load as fathomLoad } from 'fathom-client'
+import * as Fathom from 'fathom-client'
+import { useLocation } from '@remix-run/react'
 
 export default function FathomAnalytics() {
+  const location = useLocation()
+  const shouldTrack = ENV.NODE_ENV === 'production' && !ENV.E2E_TESTING
+
   useEffect(() => {
-    if (ENV.NODE_ENV === 'production' && !ENV.E2E_TESTING) {
-      fathomLoad('GSHQIEZX', {
+    if (shouldTrack) {
+      Fathom.load('GSHQIEZX', {
         url: 'https://reliable-brave.silvenon.com/script.js',
         spa: 'history',
         includedDomains: ['silvenon.com'],
       })
     }
-  }, [])
+  }, [shouldTrack])
+
+  useEffect(() => {
+    if (shouldTrack) {
+      Fathom.trackPageview()
+    }
+  }, [shouldTrack, location])
+
   return null
 }
