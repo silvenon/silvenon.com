f69ed96b5ac6e48081b9a4269dd47a567c1b24fd
Matija Marohnić <matija.marohnic@gmail.com>
2022-08-06 18:09:04 +0200

    Simplify Fathom implementation

--- app/components/Analytics.tsx
+++ app/components/Analytics.tsx
@@ -1,26 +1,24 @@
-import { useEffect } from 'react'
-import * as Fathom from 'fathom-client'
+import { load, trackPageview } from 'fathom-client'
 import { useLocation } from '@remix-run/react'
+import { useRef, useEffect } from 'react'
 
 export default function FathomAnalytics() {
   const location = useLocation()
-  const shouldTrack = ENV.NODE_ENV === 'production' && !ENV.E2E_TESTING
+  const loadedRef = useRef(false)
 
   useEffect(() => {
-    if (shouldTrack) {
-      Fathom.load('GSHQIEZX', {
+    if (ENV.NODE_ENV !== 'production' || ENV.E2E_TESTING) return
+    if (!loadedRef.current) {
+      load('GSHQIEZX', {
         url: 'https://reliable-brave.silvenon.com/script.js',
         spa: 'history',
         includedDomains: ['silvenon.com'],
       })
+    } else {
+      trackPageview()
     }
-  }, [shouldTrack])
-
-  useEffect(() => {
-    if (shouldTrack) {
-      Fathom.trackPageview()
-    }
-  }, [shouldTrack, location])
+    loadedRef.current = true
+  }, [location])
 
   return null
 }
