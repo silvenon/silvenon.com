b07ed49b9bb3f82127d8d4410b7e4eb32e170882
Matija Marohnić <matija.marohnic@gmail.com>
2022-08-07 22:09:00 +0200

    Fix tracking in client-side navigation

--- app/root.tsx
+++ app/root.tsx
@@ -136,6 +136,9 @@ function App() {
           data-site="GSHQIEZX"
           data-excluded-domains="localhost,staging.silvenon.com"
           data-spa="history"
+          // currently the canonical URLs don't change during client-side navigation, so only the
+          // initially loaded page and full refresh would be tracked, not navigating through the site
+          data-canonical="false"
           defer
         />
         <Scripts />
