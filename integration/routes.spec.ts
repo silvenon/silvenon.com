384d43f5a1bf8dd594e9777c9af9fb56fcdef302
Matija Marohnić <matija.marohnic@gmail.com>
2022-08-11 00:03:10 +0200

    Simplify test

--- integration/routes.spec.ts
+++ integration/routes.spec.ts
@@ -1,16 +1,11 @@
 import { test, expect } from '@playwright/test'
+import invariant from 'tiny-invariant'
 
 test.describe('routes', () => {
   test('https', async ({ page }) => {
     for (const route of ['/', '/blog/tailwind-and-separation-of-concerns']) {
-      const [response] = await Promise.all([
-        page.waitForEvent('response', async (response) => {
-          const contentType = await response.headerValue('content-type')
-          return contentType?.includes('text/html') || false
-        }),
-        // we only need to check the headers, so it's faster to just wait for the network response
-        page.goto(route, { waitUntil: 'commit' }),
-      ])
+      const response = await page.goto(route, { waitUntil: 'commit' })
+      invariant(response, 'response should be defined')
       expect(
         await response.headerValue('Strict-Transport-Security'),
         `Route "${route}" is missing the Strict-Transport-Security header`,
