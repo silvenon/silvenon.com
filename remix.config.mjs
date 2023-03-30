eb248cacfeba88567842527a603c2ec592ac3c76
Matija Marohnić <matija.marohnic@gmail.com>
2023-03-30 11:46:22 +0200

    Switch to using Remix's Tailwind feature

--- remix.config.mjs
+++ remix.config.mjs
@@ -4,6 +4,9 @@
 export default {
   cacheDirectory: './node_modules/.cache/remix',
   ignoredRouteFiles: ['**/*.test.{ts,tsx}'],
+  future: {
+    unstable_tailwind: true,
+  },
   serverDependenciesToBundle: [
     /^micromark.*/,
     'decode-named-character-reference',
