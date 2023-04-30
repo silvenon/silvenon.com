39d07d2a6744eb3e98d54e9a58cb7a83f24227fe
Matija Marohnić <matija.marohnic@gmail.com>
2023-04-30 22:26:23 +0200

    Adjust to the new default

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -2,7 +2,7 @@ import { PassThrough } from 'stream'
 import { renderToPipeableStream } from 'react-dom/server'
 import { RemixServer } from '@remix-run/react'
 import { Response } from '@remix-run/node'
-import type { EntryContext, Headers } from '@remix-run/node'
+import type { EntryContext } from '@remix-run/node'
 import isbot from 'isbot'
 import { getEnv } from './utils/env.server'
 
@@ -16,17 +16,36 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext,
 ) {
-  const callbackName = isbot(request.headers.get('user-agent'))
-    ? 'onAllReady'
-    : 'onShellReady'
+  return isbot(request.headers.get('user-agent'))
+    ? handleBotRequest(
+        request,
+        responseStatusCode,
+        responseHeaders,
+        remixContext,
+      )
+    : handleBrowserRequest(
+        request,
+        responseStatusCode,
+        responseHeaders,
+        remixContext,
+      )
+}
 
+function handleBotRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext,
+) {
   return new Promise((resolve, reject) => {
-    let didError = false
-
     const { pipe, abort } = renderToPipeableStream(
-      <RemixServer context={remixContext} url={request.url} />,
+      <RemixServer
+        context={remixContext}
+        url={request.url}
+        abortDelay={ABORT_DELAY}
+      />,
       {
-        [callbackName]() {
+        onAllReady() {
           const body = new PassThrough()
 
           responseHeaders.set('Content-Type', 'text/html')
@@ -38,19 +57,66 @@ export default function handleRequest(
 
           resolve(
             new Response(body, {
-              status: didError ? 500 : responseStatusCode,
               headers: responseHeaders,
+              status: responseStatusCode,
             }),
           )
 
           pipe(body)
         },
-        onShellError(err: unknown) {
-          reject(err)
+        onShellError(error: unknown) {
+          reject(error)
         },
-        onError(err: unknown) {
-          didError = true
-          console.error(err)
+        onError(error: unknown) {
+          console.error(error)
+          responseStatusCode = 500
+        },
+      },
+    )
+
+    setTimeout(abort, ABORT_DELAY)
+  })
+}
+
+function handleBrowserRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext,
+) {
+  return new Promise((resolve, reject) => {
+    const { pipe, abort } = renderToPipeableStream(
+      <RemixServer
+        context={remixContext}
+        url={request.url}
+        abortDelay={ABORT_DELAY}
+      />,
+      {
+        onShellReady() {
+          const body = new PassThrough()
+
+          responseHeaders.set('Content-Type', 'text/html')
+          // if they connect once with HTTPS, then they'll connect with HTTPS for the next hundred years
+          responseHeaders.set(
+            'Strict-Transport-Security',
+            `max-age=${60 * 60 * 24 * 365 * 100}`,
+          )
+
+          resolve(
+            new Response(body, {
+              headers: responseHeaders,
+              status: responseStatusCode,
+            }),
+          )
+
+          pipe(body)
+        },
+        onShellError(error: unknown) {
+          reject(error)
+        },
+        onError(error: unknown) {
+          console.error(error)
+          responseStatusCode = 500
         },
       },
     )
